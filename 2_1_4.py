d9c34d9fa423a5f2f5ebbdfd8d1355b9411303f3
oscar1234456 <chen.oscar@hotmail.com>
2020-03-14 15:09:43 +0800

    Complete 2_1_4

---
+++ 2_1_4.py
@@ -0,0 +1,17 @@
+a = eval(input())
+b = eval(input())
+op = input()
+
+if op == "+":
+    print(a+b)
+elif op == "-":
+    print(a-b)
+elif op == "*":
+    print(a*b)
+elif op == "/":
+    print(a/b)
+elif op == "//":
+    print(a//b)
+elif op == "%":
+    print(a % b)
+
